8b407af8a0191a20bded009138d733141664f661
DefGh <defghq@gmail.com>
2024-09-04 14:42:47 +0400

    Added regex validator to rule

--- test/FluentImporter.Tests/FileDataImportRule.cs
+++ test/FluentImporter.Tests/FileDataImportRule.cs
@@ -6,10 +6,22 @@ public class FileDataImportRule : ImportRule<FileData>
 {
     public FileDataImportRule()
     {
-        RuleFor(x => x.Name).ReadFromColumn("Name");
-        RuleFor(x => x.Description).ReadFromColumn("Description");
-        RuleFor(x => x.Date).ReadFromColumn("Date").Convert(DateTime.Parse);
-        RuleFor(x => x.Comment).ReadFromColumn("Comment");
-        RuleFor(x => x.Id).ReadFromColumn("Id").Convert(long.Parse);
+        RuleFor(x => x.Name)
+            .ReadFromColumn("Name");
+        
+        RuleFor(x => x.Description)
+            .ReadFromColumn("Description");
+        
+        RuleFor(x => x.Date)
+            .ReadFromColumn("Date")
+            .Convert(DateTime.Parse);
+        
+        RuleFor(x => x.Comment)
+            .ReadFromColumn("Comment");
+        
+        RuleFor(x => x.Id)
+            .ReadFromColumn("Id")
+            .Validate("^[0-9]*$")
+            .Convert(long.Parse);
     }
 }
